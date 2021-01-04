1ef142060c95233b4a351e05f7649193580d16b0
Pradeet Swamy <pradeet1995@gmail.com>
2021-01-04 20:17:45 +0530

    fixing inset core-plugin.

--- corePlugins/inset.js
+++ corePlugins/inset.js
@@ -1,6 +1,17 @@
 import generator from '../util/generator';
 
-export default ({theme}) => generator.generate('inset', ['top', 'bottom', 'left', 'right'], theme.inset, [
-    ['x', ['left', 'right']],
-    ['y', ['top', 'bottom']],
-]);
+import top from './top';
+import bottom from './bottom';
+import left from './left';
+import right from './right';
+
+export default ({theme}) => ({
+    ...generator.generate('inset', ['top', 'bottom', 'left', 'right'], theme.inset, [
+        ['x', ['left', 'right']],
+        ['y', ['top', 'bottom']],
+    ]),
+    ...top({ theme }),
+    ...bottom({ theme }),
+    ...left({ theme }),
+    ...right({ theme }),
+});
